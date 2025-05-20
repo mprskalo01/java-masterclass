b0f46151c9cd20409c48b6b649738c4edab2d5a5
mprskalo01 <mario.prskalo5@gmail.com>
2025-05-20 09:51:37 +0200

    learn: statements, whitespace and code organization

---
+++ section5/StatementsWhitespaceAndIndenting/src/main/java/org/mprskalo/Main.java
@@ -0,0 +1,18 @@
+package org.mprskalo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+
+        System.out.println("Hello, World! " + myVariable);
+
+        if (myVariable == 0) {
+            System.out.println("Hello, World!");
+        }
+
+    }
+}
