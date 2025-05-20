29ff3f276edcdbfa5d73123f13176426aafcff22
mprskalo01 <mario.prskalo5@gmail.com>
2025-05-20 13:50:17 +0200

    learn: if statements and code blocks

---
+++ section5/IfKeywordAndCodeBlock/src/main/java/org/mprskalo/Main.java
@@ -0,0 +1,19 @@
+package org.mprskalo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 5000) {
+            System.out.println("You win! Your score is: 5000");
+        } else {
+            System.out.println("Got here");
+        }
+    }
+}
